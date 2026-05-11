8fc233a95878ea4fc30c61493287dda4d4e53192
wafspaul <onlinelearning366@gmail.com>
2026-05-11 14:43:35 +0300

    feat: add Vercel Analytics + Google Search Console verification

--- index.tsx
+++ index.tsx
@@ -1,8 +1,11 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { inject } from '@vercel/analytics';
 import App from './App';
 import './index.css';
+
+inject();
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
